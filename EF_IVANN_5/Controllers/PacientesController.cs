86aae1f9076af083eabe740e471cf3da6e260c2e
Centene <jgilrodrigo@hotmail.com>
2016-12-30 22:26:26 +0100

    Primer cambio

---
+++ EF_IVANN_5/Controllers/PacientesController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EF_IVANN_5.Controllers
+{
+    public class PacientesController : ApiController
+    {
+    }
+}
