51abc77e6c98cddfbd244114f257ff0951355076
Centene <jgilrodrigo@hotmail.com>
2016-12-31 12:01:30 +0100

    Proyecto ya con vista y funciona se la pasa el IDPACIENTE al HttpPost

--- EF_IVANN_5/Controllers/PacientesController.cs
+++ EF_IVANN_5/Controllers/PacientesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EF_IVANN_5.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,20 @@ namespace EF_IVANN_5.Controllers
 {
     public class PacientesController : Controller
     {
+        Pacientes paciente = new Pacientes();
+        CabeceraFras factura = new CabeceraFras();
         // GET: Pacientes
         public ActionResult Index()
         {
+            CabFrasGlobal PACI_FRA = new CabFrasGlobal();
+            PACI_FRA.PacientesGlob = paciente.GetALLPacientes().ToList();
+            PACI_FRA.FacturasGlob = factura.GetAllFras().ToList();
+            return View(PACI_FRA);
+        }
+        [HttpPost]
+        public ActionResult Index(Pacientes info)
+        {
+            var id = info.IDPACIENTE;
             return View();
         }
     }
