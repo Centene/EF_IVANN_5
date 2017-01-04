f21f03e2bbd1ebc8f45aed941f838c08f8aa99b8
Centene <jgilrodrigo@hotmail.com>
2017-01-04 18:54:14 +0100

    Ya con edicion de factura: el TextHidden es preciso para pasar en el modelo la IDLINEAFFRA

--- EF_IVANN_5/Controllers/CabeceraFrasController.cs
+++ EF_IVANN_5/Controllers/CabeceraFrasController.cs
@@ -1,6 +1,7 @@
 ﻿using EF_IVANN_5.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -20,15 +21,40 @@ namespace EF_IVANN_5.Controllers
             return View();
 
         }
-
+        
         public ActionResult Edit(int? id)
         {
             IVANN_Entities db = new IVANN_Entities();
             CabeceraFras factura = new CabeceraFras();
             int idlineafactura = Convert.ToInt32(id);
+            //factura = db.CabeceraFras.Find(idlineafactura);
             var factu = db.CabeceraFras.FirstOrDefault(acc => acc.IDLINEAFRA == idlineafactura);
+
             return View(factu);
 
         }
+        [HttpPost]
+
+        public ActionResult Edit(CabeceraFras model)
+        {
+            IVANN_Entities db = new IVANN_Entities();
+                
+            CabeceraFras factura_mod = new CabeceraFras();
+            factura_mod = db.CabeceraFras.Find(model.IDLINEAFRA);
+            factura_mod.TOTAL = model.TOTAL;
+            UpdateModel(factura_mod);
+            try
+            {
+                db.SaveChanges();
+            }
+            catch(DbEntityValidationException ex)
+            {
+                string cadena;
+            }
+
+            return View();
+        } 
+
+
     }
 }
